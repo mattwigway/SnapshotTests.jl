45ceee89326a23e9504b75e615ed46358f63c246
Matt Bhagat-Conway <matt@indicatrix.org>
2023-05-09 15:23:53 -0400

    Files generated by PkgTemplates
    PkgTemplates version: 0.7.29

---
+++ src/SnapshotTests.jl
@@ -0,0 +1,5 @@
+module SnapshotTests
+
+# Write your package code here.
+
+end
---
+++ test/runtests.jl
@@ -0,0 +1,6 @@
+using SnapshotTests
+using Test
+
+@testset "SnapshotTests.jl" begin
+    # Write your tests here.
+end
